ae72ef3e6bc8350076b0aca8279e0ba3f736eea7
Chisanan232 <chi10211201@cycu.org.tw>
2025-11-28 14:50:45 +0800

    ➕ Add a new module about the core logic of MCP client features.

--- gearmeshing_ai/mcp_client/__init__.py
+++ gearmeshing_ai/mcp_client/__init__.py
@@ -1,7 +1,9 @@
+from .client import MCPClient
 from .config import MCPConfig
 from .models import ToolMetadata, ToolResult
 
 __all__ = [
+    "MCPClient",
     "MCPConfig",
     "ToolMetadata",
     "ToolResult",
