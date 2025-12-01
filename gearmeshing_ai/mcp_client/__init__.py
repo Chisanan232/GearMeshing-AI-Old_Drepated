6ddc616bdfcbd6db92cb415c1c845ded633e0a5e
Chisanan232 <chi10211201@cycu.org.tw>
2025-12-01 12:22:12 +0800

    ✏️ Adjust the export paths in init module.

--- gearmeshing_ai/mcp_client/__init__.py
+++ gearmeshing_ai/mcp_client/__init__.py
@@ -1,9 +1,7 @@
-from .client import MCPClient
-from .config import MCPConfig
-from .models import ToolMetadata, ToolResult
+from .schemas.config import MCPConfig
+from .schemas.dto import ToolMetadata, ToolResult
 
 __all__ = [
-    "MCPClient",
     "MCPConfig",
     "ToolMetadata",
     "ToolResult",
