d582ecc2da8c6354b4df172684d8c84f9ff989be
Chisanan232 <chi10211201@cycu.org.tw>
2025-12-01 10:01:30 +0800

    ➕ Add customzied errors for the MCP gateway tool API client.

--- gearmeshing_ai/mcp_client/gateway_api/__init__.py
+++ gearmeshing_ai/mcp_client/gateway_api/__init__.py
@@ -1,7 +1,10 @@
 from .models import GatewayServer, GatewayServerCreate, GatewayTransport
+from .errors import GatewayApiError, GatewayServerNotFoundError
 
 __all__ = [
     "GatewayServer",
     "GatewayServerCreate",
     "GatewayTransport",
+    "GatewayApiError",
+    "GatewayServerNotFoundError",
 ]
