ea4be70f48f239e5a22ac57de7f28f6a845bf8bb
Chisanan232 <chi10211201@cycu.org.tw>
2025-12-01 09:45:28 +0800

    ♻️ Refine the init module of data models sub-package.

--- gearmeshing_ai/mcp_client/schemas/__init__.py
+++ gearmeshing_ai/mcp_client/schemas/__init__.py
@@ -1,11 +1,8 @@
-from .dto import ToolMetadata, ToolResult, JSONRPCRequest, JSONRPCError, JSONRPCResponse
 from .config import MCPConfig
+from .dto import ToolMetadata, ToolResult
 
 __all__ = [
+    "MCPConfig",
     "ToolMetadata",
     "ToolResult",
-    "JSONRPCRequest",
-    "JSONRPCError",
-    "JSONRPCResponse",
-    "MCPConfig",
 ]
