f840be13db121bb6e64e3b1a40c7f68a79c01b05
Chisanan232 <chi10211201@cycu.org.tw>
2025-12-15 13:32:45 +0800

    ➕ Add a new prompt provider which has versioning feature.

--- gearmeshing_ai/info_provider/prompt/__init__.py
+++ gearmeshing_ai/info_provider/prompt/__init__.py
@@ -1,8 +1,10 @@
 from .builtin import BuiltinPromptProvider
 from .provider import PromptProvider
+from .stacked import StackedPromptProvider
 
 __all__ = [
     "PromptProvider",
     "BuiltinPromptProvider",
+    "StackedPromptProvider",
 ]
 
