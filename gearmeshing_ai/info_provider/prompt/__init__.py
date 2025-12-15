634c8315752b3e2e470ffa0025f4653deede5ee6
Chisanan232 <chi10211201@cycu.org.tw>
2025-12-15 13:33:18 +0800

    ➕ Add a new prompt provider which has hot loading feature.

--- gearmeshing_ai/info_provider/prompt/__init__.py
+++ gearmeshing_ai/info_provider/prompt/__init__.py
@@ -1,10 +1,12 @@
 from .builtin import BuiltinPromptProvider
 from .provider import PromptProvider
+from .reload import HotReloadWrapper
 from .stacked import StackedPromptProvider
 
 __all__ = [
     "PromptProvider",
     "BuiltinPromptProvider",
     "StackedPromptProvider",
+    "HotReloadWrapper",
 ]
 
