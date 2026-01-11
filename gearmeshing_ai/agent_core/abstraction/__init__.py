10053133dfa730219e7e08c98abc04d32143a817
Chisanan232 <chi10211201@cycu.org.tw>
2026-01-11 19:51:45 +0800

    ➕ Add a new module about the AI agent instance cache mechanism of the abstractions.

--- gearmeshing_ai/agent_core/abstraction/__init__.py
+++ gearmeshing_ai/agent_core/abstraction/__init__.py
@@ -13,9 +13,11 @@ Key Components:
 """
 
 from .base import AIAgentBase, AIAgentConfig, AIAgentResponse
+from .cache import AIAgentCache
 
 __all__ = [
     "AIAgentBase",
     "AIAgentConfig",
     "AIAgentResponse",
+    "AIAgentCache",
 ]
