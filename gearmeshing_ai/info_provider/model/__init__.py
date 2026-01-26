ccbf8f72bccbdffe4181c7e7782db0ab9f0f8272
Chisanan232 <chi10211201@cycu.org.tw>
2026-01-26 09:33:14 +0800

    ➕ Add a new sub-package for the AI model detail settings provider.

---
+++ gearmeshing_ai/info_provider/model/__init__.py
@@ -0,0 +1,5 @@
+"""Model provider package initialization.
+
+This module provides a convenient facade for accessing model provider
+functionality, mirroring the structure of the prompt provider package.
+"""
